87d58c27a831d006c4f8922ba7327fb48bd56981
camooney <camooney@tcd.4c16>
2018-09-14 09:42:11 +0000

    Added test file.

---
+++ test.py
@@ -0,0 +1,7 @@
+def say_stuff():
+        print("some day")
+        print("when the world is cold")
+        print("i will feel a glow")
+        print("just thinking of you")
+        print("and the way you look tonight")
+        print("this is a creepy quote")
